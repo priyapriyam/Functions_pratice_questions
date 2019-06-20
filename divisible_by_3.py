1be5abd7028e093a8f184fefa0f137d304edb96d
priyapriyam <priyam18@navgurukul.org>
2019-06-20 17:04:45 +0530

    In this question i have to find out maximum number .so i did this question in function

---
+++ divisible_by_3.py
@@ -0,0 +1,7 @@
+def divisible_by_three(n):
+    var=1
+    while var <= n:
+        if var%3==0:
+            print (var)
+        var=var+1
+divisible_by_three(50)
---
+++ maximum_number.py
@@ -0,0 +1,11 @@
+def max_number(a,b,c):
+    if a>b:
+        print(a)
+    elif b>c:
+        print(b)
+    else:
+        print(c)
+max_number(29,20,8)
+        
+    
+   
